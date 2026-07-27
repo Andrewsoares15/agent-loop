db057b256475a1a05b72395943f4b9fda5498288
Andrewsoares15 <andrewsoares347@gmail.com>
2026-07-27 15:08:46 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -54,7 +54,7 @@ def main():
                if tc.function.name == "Read":
                    with open(path, "r", encoding="utf-8") as file:
                        content = file.read()
-                       messages.append({role: "user", content: content})
+                       messages.append({"role": "user", "content": content})
 
                print(content)
 if __name__ == "__main__":
