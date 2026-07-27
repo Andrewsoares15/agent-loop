413c774bec1dd300925d7f58f9630d6d14031a4a
Andrewsoares15 <andrewsoares347@gmail.com>
2026-07-27 20:37:40 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -105,7 +105,7 @@ def main():
                 except Exception as e:
                     tool_response = f"Error writing file: {str(e)}"
             elif tc.function.name == "Bash":
-                tool_response = subprocess.run([arguments_dict["command"], capture_output=True, text=True)
+                tool_response = subprocess.run([arguments_dict["command"]], capture_output=True, text=True)
             messages.append({"role": "tool","tool_call_id": tc.id, "content": tool_response, "name": "Write"})
 
 
