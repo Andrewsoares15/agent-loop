07a996601bbeaa705a717535833a5541e4dd289e
Andrewsoares15 <andrewsoares347@gmail.com>
2026-07-27 15:22:55 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ def main():
         print("Logs from your program will appear here!", file=sys.stderr)
         r = chat.choices[0]
         messages.append(r.message)
-        if r.message.tool_calls and len(r.message.tool_calls) > 0:
+        if !r.message.tool_calls:
             print(r.message.content)
             break
 
