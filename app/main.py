6de2157333425b2fb35088e89ab39ab8629e02bd
Andrewsoares15 <andrewsoares347@gmail.com>
2026-07-27 22:09:51 -0300

    small refactor

--- app/main.py
+++ app/main.py
@@ -81,7 +81,6 @@ def main():
         if not chat.choices or len(chat.choices) == 0:
             raise RuntimeError("no choices in response")
 
-        # You can use print statements as follows for debugging, they'll be visible when running tests.
         print("Logs from your program will appear here!", file=sys.stderr)
 
         r = chat.choices[0]
